04acd93a652c1602b3f67165fd0f188f0277cd75
konhi <hello.konhi@gmail.com>
2022-02-26 15:49:05 +0100

    navigation

---
+++ pages/settings.tsx
@@ -0,0 +1,22 @@
+import styles from "../styles/Home.module.css";
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <AppBar position="fixed">
+      <Toolbar>
+      <Link href="/">
+
+      <IconButton href="index" size="large" edge="start" color="inherit" aria-label="menu">
+        <ArrowBackIcon />
+      </IconButton>
+      </Link>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Ustawienia
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+}
