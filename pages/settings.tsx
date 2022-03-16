8e2195d18bc730b99e842e05caab4226ec393e52
Krystian Wybranowski <krystian.wybranowski@gmail.com>
2022-03-16 18:33:59 +0100

    ⚙ settings ui

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,22 +1,71 @@
 import styles from "../styles/Home.module.css";
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import {
+  AppBar,
+  Box,
+  Button,
+  FormControl,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <AppBar position="fixed">
-      <Toolbar>
-      <Link href="/" passHref>
+  const [energyLimit, setEnergyLimit] = useState(0);
 
-      <IconButton href="index" size="large" edge="start" color="inherit" aria-label="menu">
-        <ArrowBackIcon />
-      </IconButton>
-      </Link>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Ustawienia
-        </Typography>
-      </Toolbar>
-    </AppBar>
+  useEffect(() => {
+    const savedEnergyLimit = localStorage.getItem("energyLimit");
+    if (savedEnergyLimit){
+      setEnergyLimit(savedEnergyLimit);
+    }
+  }, []);
+
+  const handleEnergyLimitChange = () => {
+    console.log(energyLimit);
+    localStorage.setItem("energyLimit", energyLimit);
+  };
+  
+  return (
+    <>
+      <AppBar position="sticky">
+        <Toolbar>
+          <Link href="/" passHref>
+            <IconButton
+              href="index"
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+            >
+              <ArrowBackIcon />
+            </IconButton>
+          </Link>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Ustawienia
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box sx={{ display: "flex", flexDirection: "column", flexWrap: "wrap", alignItems: "center" }}>
+        <h3>Ustaw dzienny limit zużycia prądu</h3>
+        <FormControl sx={{ width: "200px", margin: "auto" }} variant="standard">
+          <InputLabel htmlFor="standard-adornment-amount">Limit Zużycia energii</InputLabel>
+          <Input
+            type="number"
+            value={energyLimit}
+            onChange={(e) => setEnergyLimit(e.target.value)}
+            id="standard-adornment-amount"
+            startAdornment={
+              <InputAdornment position="start">kWh</InputAdornment>
+            }
+          />
+          <Button sx={{marginTop: "10px"}} variant="contained" size="small" onClick={() => handleEnergyLimitChange()}>zmień</Button>
+        </FormControl>
+      </Box>
+    </>
   );
 }
